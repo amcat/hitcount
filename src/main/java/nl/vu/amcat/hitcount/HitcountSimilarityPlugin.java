a4e6d6eaa61585e26df2f8792616b793360a3e54
Christian <CJStuart@users.noreply.github.com>
2017-05-10 15:27:59 +0200

    Removed commented out code

--- src/main/java/nl/vu/amcat/hitcount/HitcountSimilarityPlugin.java
+++ src/main/java/nl/vu/amcat/hitcount/HitcountSimilarityPlugin.java
@@ -4,16 +4,6 @@ import org.elasticsearch.index.IndexModule;
 import org.elasticsearch.plugins.Plugin;
 
 public class HitcountSimilarityPlugin extends Plugin{
-/*    @Override
-    public String name() {
-        return "hitcount";
-    }
-
-    @Override
-    public String description() {
-        return "Hitcount Similarity Plugin";
-    }
-*/
     @Override
     public void onIndexModule(final IndexModule module) {
         module.addSimilarity("hitcountsimilarity", (name, settings) -> new HitcountSimilarityProvider(name, settings));
